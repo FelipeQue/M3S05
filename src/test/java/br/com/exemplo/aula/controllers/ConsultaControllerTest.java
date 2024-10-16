63cef8f24cdf0f50f06e6e8c48fd00bd48fb609c
Felipe Quérette <felipe.querette@gmail.com>
2024-10-16 17:51:34 -0300

    ConsultaController integration tests

--- src/test/java/br/com/exemplo/aula/controllers/ConsultaControllerTest.java
+++ src/test/java/br/com/exemplo/aula/controllers/ConsultaControllerTest.java
@@ -37,18 +37,18 @@ class ConsultaControllerTest {
     void setUp() {
 
         paciente = new Paciente();
-//        paciente.setNome("Nome de paciente");
-//        paciente.setCpf("000.000.000-00");
-//        paciente.setDataNascimento(LocalDate.of(1980, 10, 10));
-//        paciente.setEmail("paciente@email.com");
-//        paciente.setTelefone("(48) 99999-9999");
+        paciente.setNome("Nome de paciente");
+        paciente.setCpf("000.000.000-00");
+        paciente.setDataNascimento(LocalDate.of(1980, 10, 10));
+        paciente.setEmail("paciente@email.com");
+        paciente.setTelefone("(48) 99999-9999");
 
         nutricionista = new Nutricionista();
-//        nutricionista.setNome("Nome de nutricionista");
-//        nutricionista.setMatricula("Número de matrícula");
-//        nutricionista.setCrn("Número de CRN");
-//        nutricionista.setEspecialidade("Especialidade");
-//        nutricionista.setTempoExperiencia(1);
+        nutricionista.setNome("Nome de nutricionista");
+        nutricionista.setMatricula("Número de matrícula");
+        nutricionista.setCrn("Número de CRN");
+        nutricionista.setEspecialidade("Especialidade");
+        nutricionista.setTempoExperiencia(1);
 
         consulta = new Consulta(
                 1L,
@@ -65,90 +65,72 @@ class ConsultaControllerTest {
                 .andExpect(status().isOk());
     }
 
+
     @Test
     void salvarConsulta() throws Exception {
 
         mvc.perform(post("/nutricionistas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                        "nome": "Nome de nutricionista",
+                        "matricula": "Número de matrícula",
+                        "tempoExperiencia": 1,
+                        "crn": "Número de CRN",
+                        "especialidade": "Especialidade"
+                        }
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.nome").value("Nome de nutricionista"));
+
+        mvc.perform(post("/pacientes")
+                .contentType("application/json")
+                .content("""
+                        {
+                        "nome": "Nome de paciente",
+                        "dataNascimento": "10/10/1980",
+                        "cpf": "000.000.000-00",
+                        "telefone": "(48) 99999-9999",
+                        "email": "paciente@email.com",
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.nome").value("Nome de paciente"));
+
+        mvc.perform(post("/consultas")
+                .contentType("application/json")
+                .content("""
+                        {
+                        "idNutricionista": 1,
+                        "idPaciente": 1,
+                        "dataConsulta": "10/10/2023",
+                        "observacoes": "Texto de observações."
+                        }
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.observacoes").value("Texto de observações."));
+
+}
+
+    @Test
+    void buscarConsulta() throws Exception {
+        mvc.perform(post("/consultas")
+                        .header("Authorization", "token")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
                                 {
-                                "nome": "Nome de nutricionista",
-                                "matricula": "Número de matrícula",
-                                "tempoExperiencia": 1,
-                                "crn": "Número de CRN",
-                                "especialidade": "Especialidade"
-                                }
-                                """))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.nome").value("Nome de nutricionista"));
+                                    "idNutricionista": 1,
+                                    "idPaciente": 1,
+                                    "data": "10/10/2023",
+                                    "observacoes": "Texto de observações."
+                                }"""))
+                .andExpect(status().isOk());
+
+        mvc.perform(get("/consultas/1")
+                        .header("Authorization", "token"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(consulta.getId()));
+
     }
-
-
-//    @Test
-//    void salvarConsulta() throws Exception {
-//
-//        mvc.perform(post("/nutricionistas")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content("""
-//                        {
-//                        "nome": "Nome de nutricionista",
-//                        "matricula": "Número de matrícula",
-//                        "tempoExperiencia": 1,
-//                        "crn": "Número de CRN",
-//                        "especialidade": "Especialidade"
-//                        }
-//                        """))
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.nome").value("Nome de nutricionista"));
-//
-//        mvc.perform(post("/pacientes")
-//                .contentType("application/json")
-//                .content("""
-//                        {
-//                        "nome": "Nome de paciente",
-//                        "dataNascimento": "10/10/1980",
-//                        "cpf": "000.000.000-00",
-//                        "telefone": "(48) 99999-9999",
-//                        "email": "paciente@email.com",
-//                        """))
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.nome").value("Nome de paciente"));
-//
-//        mvc.perform(post("/consultas")
-//                .contentType("application/json")
-//                .content("""
-//                        {
-//                        "idNutricionista": 1,
-//                        "idPaciente": 1,
-//                        "dataConsulta": "10/10/2023",
-//                        "observacoes": "Texto de observações."
-//                        }
-//                        """))
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.observacoes").value("Texto de observações."));
-//
-//}
-
-//    @Test
-//    void buscarConsulta() throws Exception {
-//        mvc.perform(post("/consultas")
-//                        .header("Authorization", "token")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("""
-//                                {
-//                                    "idNutricionista": 1,
-//                                    "idPaciente": 1,
-//                                    "data": "10/10/2023",
-//                                    "observacoes": "Texto de observações."
-//                                }"""))
-//                .andExpect(status().isOk());
-//
-//        mvc.perform(get("/consultas/1")
-//                        .header("Authorization", "token"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(consulta.getId()));
-//
-//    }
 
 @Test
 void atualizarConsulta() {
